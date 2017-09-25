45d78e1d292f9de2e5f4c5b1a3fec33626065af2
Paul <paul.huynh3@gmail.com>
2017-09-25 12:02:29 -0400

    created url get request

--- Github Repos/Github Repos/ViewController.m
+++ Github Repos/Github Repos/ViewController.m
@@ -18,7 +18,20 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    NSURL *url = [NSURL URLWithString:@"https://api.github.com/users/paulhuynh3/repos"];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
     
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration];
+    
+    NSURLSessionDataTask *dataTask = [session  dataTaskWithRequest:urlRequest completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+       
+        
+        
+    }];
+    
+    //The data task is created in a suspended state so we need to resume it
+    [dataTask resume];
     
 }
 
